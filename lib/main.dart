07b4b640cd1b6509b7ab154654bcfbaf93f37c76
StelaX10 <PhysicsChampion21@gmail.com>
2021-08-31 02:44:19 +0200

    Edits on Mariam Push

--- lib/main.dart
+++ lib/main.dart
@@ -2,54 +2,63 @@ import 'dart:ui';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'SignUp_SignIn/splash_screen1.dart';
 
-void main() {
+void main() 
+{
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget 
+{
   @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Homepage',
-      theme: ThemeData(
-          fontFamily: 'spoqa',
-        //gives a theme for the whole app design
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          inputDecorationTheme: InputDecorationTheme(
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(),
-              )
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
+      title: 'SIDA - Egyptian Ride Hailing App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData
+      (
+        //fontFamily: 'spoqa',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        inputDecorationTheme:InputDecorationTheme
+        (
+          focusedBorder: UnderlineInputBorder
+          (
+            borderSide: BorderSide(),
           )
+        )
       ),
       home: WelcomePage(),
     );
   }
 }
-class WelcomePage extends StatefulWidget {
+
+class WelcomePage extends StatefulWidget
+{
   @override
   _WelcomePageState createState() => _WelcomePageState();
 }
-class _WelcomePageState extends State<WelcomePage> {
+
+class _WelcomePageState extends State<WelcomePage>
+{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) 
+  {
     final screenHeight= MediaQuery.of(context).size.height;
     final screenWidth= MediaQuery.of(context).size.width;
     return Scaffold(
-      body:   AnimatedSplashScreen(
+      body: AnimatedSplashScreen(
           splash:SvgPicture.asset('assets/SIDALogo&NameSmaller.svg',width: 20,height: 20),
           nextScreen: splashscreen1(),
           splashTransition: SplashTransition.rotationTransition,
           duration: 1000,
         backgroundColor: HexColor("2C2B62").withOpacity(0.923),
       ),
-
     );
   }
 }
