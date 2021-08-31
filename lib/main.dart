67af23b66c648166d5e0317b7a231221cbef3409
WeaamMohamed <54490984+WeaamMohamed@users.noreply.github.com>
2021-08-31 09:05:57 +0200

    edit profile UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:sida_app/screens/edit_profile_screen.dart';
 import 'screens/home_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -51,14 +52,15 @@ class _WelcomePageState extends State<WelcomePage>
   {
     final screenHeight= MediaQuery.of(context).size.height;
     final screenWidth= MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: AnimatedSplashScreen(
-          splash:SvgPicture.asset('assets/SIDALogo&NameSmaller.svg',width: 20,height: 20),
-          nextScreen: splashscreen1(),
-          splashTransition: SplashTransition.rotationTransition,
-          duration: 1000,
-        backgroundColor: HexColor("2C2B62").withOpacity(0.923),
-      ),
-    );
+    return EditProfileScreen();
+    // return Scaffold(
+    //   body: AnimatedSplashScreen(
+    //       splash:SvgPicture.asset('assets/SIDALogo&NameSmaller.svg',width: 20,height: 20),
+    //       nextScreen: splashscreen1(),
+    //       splashTransition: SplashTransition.rotationTransition,
+    //       duration: 1000,
+    //     backgroundColor: HexColor("2C2B62").withOpacity(0.923),
+    //   ),
+    // );
   }
 }
